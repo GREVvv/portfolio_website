a2fae9cb3f25437e299ef809b9be4c9583344a07
Aidan <a.grevemberg.20@abdn.ac.uk>
2023-07-25 23:51:44 +0100

    Set up project and styles

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Nav from './components/nav/Nav'
+
+const App = () => {
+  return (
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Contact/>
+    </>
+  )
+}
+
+export default App
